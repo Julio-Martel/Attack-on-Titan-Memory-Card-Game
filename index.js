7a66ec1b43be46c22ef77b9b62a47e0a5ab9e849
Diocleniax789 <julioramdom123@gmail.com>
2025-02-09 21:00:51 -0300

    se ha arreglado el problema de las cartas

--- index.js
+++ index.js
@@ -9,6 +9,311 @@ overCardSound.load();
 cardPut.load();
 gameOver.load();
 
+
+async function cargarContenido() {
+	let tableroCartas = [[9,3,1,8],
+						 [8,5,7,2],
+						 [1,2,7,10],
+						 [3,5,10,9]];	
+
+		contenidoPrincipal.innerHTML = `
+		<div class = "contenedor-tablero-estadisticas">	
+					<div class = "tablero-cartas" id = "tabla">
+						
+						<div class = "casilla" data-ejeX = "0" data-ejeY = "0" id = "0-0" data-coordenada = "0-0">
+							<img src = "images/reverso-carta.jpg" class = "carta-reverso">
+						</div>
+						<div class = "casilla" data-ejeX = "0" data-ejeY = "1" id = "0-1" data-coordenada = "0-1">
+							<img src = "images/reverso-carta.jpg" class = "carta-reverso">
+						</div>
+						<div class = "casilla" data-ejeX = "0" data-ejeY = "2" id = "0-2" data-coordenada = "0-2">
+							<img src = "images/reverso-carta.jpg" class = "carta-reverso">
+						</div>
+						<div class = "casilla" data-ejeX = "0" data-ejeY = "3" id = "0-3" data-coordenada = "0-3">
+							<img src = "images/reverso-carta.jpg" class = "carta-reverso">					
+						</div>
+						
+						<div class = "casilla" data-ejeX = "1" data-ejeY = "0" id = "1-0" data-coordenada = "1-0">
+							<img src = "images/reverso-carta.jpg" class = "carta-reverso">					
+						</div>						
+						<div class = "casilla" data-ejeX = "1" data-ejeY = "1" id = "1-1" data-coordenada = "1-1">
+							<img src = "images/reverso-carta.jpg" class = "carta-reverso">					
+						</div>
+						<div class = "casilla" data-ejeX = "1" data-ejeY = "2" id = "1-2" data-coordenada = "1-2">
+							<img src = "images/reverso-carta.jpg" class = "carta-reverso">					
+						</div>
+						<div class = "casilla" data-ejeX = "1" data-ejeY = "3" id = "1-3" data-coordenada = "1-3">
+							<img src = "images/reverso-carta.jpg" class = "carta-reverso">						
+						</div>
+
+						<div class = "casilla" data-ejeX = "2" data-ejeY = "0" id = "2-0" data-coordenada = "2-0">
+							<img src = "images/reverso-carta.jpg" class = "carta-reverso">					
+						</div>						
+						<div class = "casilla" data-ejeX = "2" data-ejeY = "1" id = "2-1" data-coordenada = "2-1">
+							<img src = "images/reverso-carta.jpg" class = "carta-reverso">					
+						</div>
+						<div class = "casilla" data-ejeX = "2" data-ejeY = "2" id = "2-2" data-coordenada = "2-2">
+							<img src = "images/reverso-carta.jpg" class = "carta-reverso">					
+						</div>
+						<div class = "casilla" data-ejeX = "2" data-ejeY = "3" id = "2-3" data-coordenada = "2-3">
+							<img src = "images/reverso-carta.jpg" class = "carta-reverso">						
+						</div>						
+						
+						<div class = "casilla" data-ejeX = "3" data-ejeY = "0" id = "3-0" data-coordenada = "3-0">
+							<img src = "images/reverso-carta.jpg" class = "carta-reverso">					
+						</div>						
+						<div class = "casilla" data-ejeX = "3" data-ejeY = "1" id = "3-1" data-coordenada = "3-1">
+							<img src = "images/reverso-carta.jpg" class = "carta-reverso">					
+						</div>
+						<div class = "casilla" data-ejeX = "3" data-ejeY = "2" id = "3-2" data-coordenada = "3-2">
+							<img src = "images/reverso-carta.jpg" class = "carta-reverso">					
+						</div>
+						<div class = "casilla" data-ejeX = "3" data-ejeY = "3" id = "3-3" data-coordenada = "3-3">
+							<img src = "images/reverso-carta.jpg" class = "carta-reverso">						
+						</div>						
+
+					</div>
+
+						<div class = "ventana-modal">
+								<div class = "contenido-modal">
+								</div>
+						</div>	
+
+					<div class = "estadisticas-vidas" id = "est-vid">
+		
+						<div class = "display-vidas" id = "vidas">
+							<div class = "display-vidas-img" id = "vidas">
+								<label for="vidas" class="entrada-vidas">Vidas:</label>
+   					  			<div class = "vidas-img" id = "vidas-img-contenedor">
+									<img src = "images/vida1.png" class = "vida-img" id = "vida1">
+									<img src = "images/vida1.png" class = "vida-img" id = "vida2">
+									<img src = "images/vida1.png" class = "vida-img" id = "vida3">
+									<img src = "images/vida1.png" class = "vida-img" id = "vida4">
+									<img src = "images/vida1.png" class = "vida-img" id = "vida5">
+								</div>
+							</div>
+						</div>
+
+						<div class = "display-score" id = "score">
+							<div class = "display-score-numbers">
+								<label for="scores" class="entrada-scores">Score:</label>
+								<input type="number" id="entrada-score" name="entrada-score" value = "0" readonly>
+							</div>
+						</div>
+
+						<div class = "display-video" id = "video">
+
+						</div>
+
+					</div>
+		</div>
+		`;
+
+		let displayScore = document.getElementById('entrada-score');
+		let casillas = document.querySelectorAll('.casilla');
+		let cartaProcesada = 1;
+		let valorCartaSeleccionadaA = "", valorCartaSeleccionadaB = "", FailA = "", FailB = "";
+		let imgReversoCarta = `<img src = "images/reverso-carta.jpg" class = "carta-reverso">`;
+		let contadorVidas = 5;
+		let tabla = document.getElementById('tabla');
+		let casillasBloqueadas = [];
+
+		primerVistazoTablero = async () => {
+		  for (let casilla of casillas) {
+		    let valorEjeX = parseInt(casilla.getAttribute('data-ejeX'));
+		    let valorEjeY = parseInt(casilla.getAttribute('data-ejeY'));
+		    let cartaAColocar = casilla.getAttribute('data-coordenada');
+		    let idCartaAColocar = document.getElementById(cartaAColocar);
+		    let valorDeLaCarta = tableroCartas[valorEjeX][valorEjeY];
+
+   			 if (valorDeLaCarta >= 1 && valorDeLaCarta <= 15) {
+       			 let imagenNueva = `<img src="images/${valorDeLaCarta}.jpg" class="carta-reverso" id="${valorDeLaCarta}">`;
+        		 idCartaAColocar.innerHTML = imagenNueva;
+    		 }		   
+		   }
+
+
+		 const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+
+  		  for (let casilla of casillas) {
+   			 let cartaAColocar = casilla.getAttribute('data-coordenada');
+    		 let idCartaAColocar = document.getElementById(cartaAColocar);
+
+    		 tabla.style.pointerEvents = "none";
+
+    		 await delay(500);
+    		 
+    		 tabla.style.pointerEvents = "auto";
+    		 idCartaAColocar.innerHTML = imgReversoCarta;
+    		 cardPut.play();}
+
+		};
+
+		await primerVistazoTablero();
+
+		for(casilla of casillas) {					
+			let valorEjeX = parseInt(casilla.getAttribute('data-ejeX'));
+			let valorEjeY = parseInt(casilla.getAttribute('data-ejeY'));				
+			let cartaAColocar = casilla.getAttribute('data-coordenada');
+			let idCartaAColocar = document.getElementById(cartaAColocar);
+
+			casilla.addEventListener('click', async () => {
+				cardPut.play();
+
+				let valorDeLaCarta = tableroCartas[valorEjeX][valorEjeY];
+				
+   			 	if (valorDeLaCarta >= 1 && valorDeLaCarta <= 15) {
+       				 let imagenNueva = `<img src="images/${valorDeLaCarta}.jpg" class="carta-reverso" id="${valorDeLaCarta}">`;
+        		 	idCartaAColocar.innerHTML = imagenNueva;
+    		 	}	
+
+					if (cartaProcesada === 1) { //CASO EN EL QUE EL PAR SI COINCIDE
+						valorCartaSeleccionadaA = valorDeLaCarta;
+						FailA = idCartaAColocar;
+						cartaProcesada++;
+
+						const idParentCasillaA = document.getElementById(cartaAColocar);
+						idParentCasillaA.style.pointerEvents = "none";
+
+					} else { // CASO EN EL QUE EL PAR NO COINCIDE	
+					
+						valorCartaSeleccionadaB = valorDeLaCarta;
+						FailB = idCartaAColocar;
+						cartaProcesada = 1;
+
+						const idParentCasillaB = document.getElementById(cartaAColocar);
+						idParentCasillaB.style.pointerEvents = "none"; 
+
+						const valorComparativaDeCartas = (valorA,valorB) => {
+							return new Promise((resolve,reject) => {
+								if (valorA === valorB) {
+									console.log('Resuelto: true');
+									resolve(true);
+								} else {
+									console.log('Rechazado: false');
+									reject(false);
+								} 
+							});
+						}
+
+						async function comparar() {
+
+							try {
+								
+								let resultadoComparativa = await valorComparativaDeCartas(valorCartaSeleccionadaA,valorCartaSeleccionadaB);
+
+								if (resultadoComparativa) {
+									let imagenNice = `<img src = "images/nice.jpg" class = "img-modal">`;
+									let displayNroScore = parseInt(displayScore.value);
+									let ventanaModal = document.querySelector('.ventana-modal');
+
+									displayScore.value = displayNroScore + 1000;
+
+									async function activarVentanaModal(valorA,valorB){	
+										
+										const esperarVentana = (ms) => new Promise(resolve => setTimeout(resolve, ms));	
+
+										const todasLasCasillas = document.querySelectorAll(".casilla");
+
+										await esperarVentana(1000);
+
+										todasLasCasillas.forEach(casilla => {
+											casilla.style.pointerEvents = "none";
+										});
+
+										ventanaModal.innerHTML = imagenNice;
+										ventanaModal.style.display = "flex";
+										ventanaModal.style.pointerEvents = "none";
+
+										await esperarVentana(3000);
+
+										ventanaModal.style.display = "none";
+
+									    todasLasCasillas.forEach(casilla => {								    	
+									    	if (valorA === casilla || valorB === casilla) {
+									    		casillasBloqueadas.push(casilla);
+									    	} else if(!casillasBloqueadas.includes(casilla)){
+									    		casilla.style.pointerEvents = "auto";
+									    	}
+									    });
+
+									}
+
+									activarVentanaModal(FailA,FailB);	
+								} 
+
+							} catch(error) {
+
+								const activarVentanaModalFail = (valorA,valorB) => {
+									let imagenFail = `<img src = "images/fail.jpg" class = "img-modal">`;
+									let ventanaModal = document.querySelector('.ventana-modal');
+									let contenedorVidas = document.getElementById('vidas-img-contenedor');
+
+									async function activarVentanaModalFail(valorA,valorB) {
+										
+										const esperarVentana = (ms) => new Promise(resolve => setTimeout(resolve,ms));
+
+										contenedorVidas.removeChild(contenedorVidas.lastElementChild);
+										contadorVidas--;
+
+										await esperarVentana(1000);
+
+										gameOver.play();
+										ventanaModal.innerHTML = imagenFail;
+										ventanaModal.style.display = "flex";
+										ventanaModal.style.pointerEvents = "none";
+										
+										await esperarVentana(5000);
+
+										cardPut.play();
+										ventanaModal.style.display = "none";
+										valorA.innerHTML = imgReversoCarta;
+										valorB.innerHTML = imgReversoCarta;
+										valorA.style.pointerEvents = "auto";
+										valorB.style.pointerEvents = "auto";	
+		
+										irALaPantallaPrincipal();
+									}								
+
+
+									activarVentanaModalFail(FailA,FailB);
+
+
+									async function irALaPantallaPrincipal() {
+										
+										const delay = (ms) => new Promise(resolve => setTimeout(resolve,ms));
+
+										if (contadorVidas === 0) {
+
+											await delay(2000);
+
+											contenidoPrincipal.innerHTML = `
+											<div class="titulo">
+												<img src="images/logo.png" class="play-img">
+												<button class="boton" id="play">Play!</button>
+											</div>
+											`;	
+										
+											let botonPlay = document.getElementById('play');
+											botonPlay.addEventListener('click', cargarContenido);
+										}
+									}
+								}
+
+								activarVentanaModalFail(FailA,FailB);			}
+
+							}
+
+						comparar();
+
+					}
+
+				});	}
+}
+
+
+/*
 async function cargarContenido(){
 	
 	let tableroCartas = [[0,0,0,0],
@@ -24,15 +329,15 @@ async function cargarContenido(){
 				<div class="formularios">
 					<form class="formulario">
  				   		<label for="coordenadaX1" class="entrada">Seleccione la coordenada(EjeX1): </label>
-   				  		<input type="number" min="0" max="3" id="coordenadaX1" name="coordenadaX1" placeholder="Ingresar nro de coordenada" required>
+   				  		<input type="number" min="0" max="3" id="coordenadaX1" name="coordenadaX1" placeholder="Ingresar numero" required>
     					<label for="coordenadaY1" class="entrada">Coordenada Y1 predeterminada  </label>
-   						<input type="number" min="0" max="3" id="coordenadaY1" name="coordenadaY1" placeholder="Ingresar nro de coordenada" required>
+   						<input type="number" min="0" max="3" id="coordenadaY1" name="coordenadaY1" placeholder="Ingresar numero" required>
 					</form>
 					<form class="formulario">
    						<label for="coordenadaX2" class="entrada">Seleccione la coordenada(EjeX2): </label>
-   					    <input type="number" min="0" max="3" id="coordenadaX2" name="coordenadaX2" placeholder="Ingresar nro de coordenada" required>
+   					    <input type="number" min="0" max="3" id="coordenadaX2" name="coordenadaX2" placeholder="Ingresar numero" required>
                         <label for="coordenadaY2" class="entrada">Coordenada Y2 predeterminada </label>
-                        <input type="number" min="0" max="3" id="coordenadaY2" name="coordenadaY2" placeholder="Ingresar nro de coordenada" required>
+                        <input type="number" min="0" max="3" id="coordenadaY2" name="coordenadaY2" placeholder="Ingresar numero" required>
                     </form>
 				</div>
 				<button type="button" class="boton" id="boton-formulario">Ingresar coordenadas</button>	
@@ -68,8 +373,6 @@ async function cargarContenido(){
 		</div>
 	</div>	
 	`;
-
-	selectCharacter.play();
 
 	buscarCasilla = (coordenadaX, coordenadaY) => {
 	    return new Promise((resolve) => {
@@ -183,8 +486,6 @@ async function cargarContenido(){
 
 	if (avanzarAlJuego === 16) {
 			
-		selectCharacter.stop();
-
 		contenidoPrincipal.innerHTML = `
 		<div class = "contenedor-tablero-estadisticas">	
 					<div class = "tablero-cartas" id = "tabla">
@@ -402,6 +703,8 @@ async function cargarContenido(){
   										});
 
 										ventanaModal.style.display = "none";
+										valorA.style.pointerEvents = "none";
+										valorB.style.pointerEvents = "none";
 									}
 
 									activarVentanaModal(FailA,FailB);	
@@ -487,8 +790,6 @@ async function cargarContenido(){
 
 	
 	});  }
+*/
 
 botonStart.addEventListener('click', cargarContenido);
-
-
-
